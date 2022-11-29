8d9840e3c2406aa7b2f4ed2b25353d23a249001e
yonniiii <119270799+yonniiii@users.noreply.github.com>
2022-11-29 14:36:48 +0900

    Update 1207. Unique Number of Occurrences.py

--- 1207. Unique Number of Occurrences.py
+++ 1207. Unique Number of Occurrences.py
@@ -1,11 +1,23 @@
 class Solution:
+#     def uniqueOccurrences(self, arr: List[int]) -> bool:
+#         dic = {}
+#         for i in range(len(arr)):
+#             if arr[i] in dic:          #키(숫자)가 중복되면 다음 loop
+#                 continue
+#             elif arr.count(arr[i]) in dic.values():   #값(숫자의count)가 중복되면 false 반환
+#                 return False
+#             else:
+#                 dic[arr[i]] = arr.count(arr[i])
+#         return True
+
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         dic = {}
         for i in range(len(arr)):
-            if arr[i] in dic:          #키(숫자)가 중복되면 다음 loop
+            arr_count = arr.count(arr[i])
+            if arr[i] in dic:                 #키(숫자)가 중복되면 다음 loop
                 continue
-            elif arr.count(arr[i]) in dic.values():   #값(숫자의count)가 중복되면 false 반환
+            elif arr_count in dic.values():   #값(숫자의count)가 중복되면 false 반환
                 return False
             else:
-                dic[arr[i]] = arr.count(arr[i])
+                dic[arr[i]] = arr_count
         return True
