c25c80385ecabb64da8100efcadb68dd44a30bd7
soyoun an <asysesang@gmail.com>
2022-12-05 10:24:36 +0900

    first commit

---
+++ 977. Squares of a Sorted Array.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+# time complexity : O(n)
+# space complexity : O(n)
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range (len(nums)):
+            result.append(nums[i]**2)
+        
+        result.sort()
+        return result
